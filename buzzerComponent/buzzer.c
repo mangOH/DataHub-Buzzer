ce0c7494c3cc2d050cfc131419e59f319e108a75
David Frey <dfrey@sierrawireless.com>
2019-03-12 16:22:06 -0700

    cleanup code formatting
    - replace tabs with spaces
    - opening braces on own line
    - function without params should be void
    - wrap long lines
    - consistent indentation
    - update if/else statements to always supply braces

--- buzzerComponent/buzzer.c
+++ buzzerComponent/buzzer.c
@@ -1,7 +1,7 @@
 /**
  *
  * This file provides the implementation of buzzer frequency selection, period,
- *	duty cycle and enable switch.
+ * duty cycle and enable switch.
  *
  * <hr>
  *
@@ -14,16 +14,16 @@
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(array) ((unsigned long) (sizeof (array) / sizeof ((array)[0])))
 #endif
-// #define RES_PATH_BUZZER			"buzzer"
-#define RES_PATH_ENABLE 		"buzzerenable"
-#define RES_PATH_FREQ_OUTPUT 		"frequency"
-#define RES_PATH_FREQ_INPUT 		RES_PATH_FREQ_OUTPUT "/value"
-#define RES_PATH_PERIOD_OUTPUT 		"buzzerperiod"
-#define RES_PATH_PERIOD_INPUT 		RES_PATH_PERIOD_OUTPUT "/value"
-#define RES_PATH_DC_ON_OUTPUT 		"duty-cycle-on-interval"
-#define RES_PATH_DC_ON_INPUT 		RES_PATH_DC_ON_OUTPUT "/value"
 
-#define TURN_OFF_CLKOUT			0x0
+#define RES_PATH_ENABLE        "buzzerenable"
+#define RES_PATH_FREQ_OUTPUT   "frequency"
+#define RES_PATH_FREQ_INPUT    RES_PATH_FREQ_OUTPUT "/value"
+#define RES_PATH_PERIOD_OUTPUT "buzzerperiod"
+#define RES_PATH_PERIOD_INPUT  RES_PATH_PERIOD_OUTPUT "/value"
+#define RES_PATH_DC_ON_OUTPUT  "duty-cycle-on-interval"
+#define RES_PATH_DC_ON_INPUT   RES_PATH_DC_ON_OUTPUT "/value"
+
+#define TURN_OFF_CLKOUT            0x0
 
 static const char BuzzerFreqPath[]   = "/sys/bus/i2c/drivers/rtc-pcf85063/8-0051/clkout_freq";
 
@@ -31,12 +31,12 @@ static const char BuzzerFreqPath[]   = "/sys/bus/i2c/drivers/rtc-pcf85063/8-0051
  * and PushHandler needs to be a union of different types
  */
 typedef struct actuator_DhResTypes_t {
-    const char			*OutputPath;
-    const char			*InputPath;
-    dhubIO_DataType_t		dataType;
-    const char			*units;
-    void			(*OutputHandler)(double, double, void *);
-    void			*ref; /* Need a union here */
+    const char        *OutputPath;
+    const char        *InputPath;
+    dhubIO_DataType_t dataType;
+    const char        *units;
+    void              (*OutputHandler)(double, double, void *);
+    void              *ref;     /* Need a union here */
 } actuator_DhResTypes_t;
 
 static void FreqConfigPushHandler (double timestamp, double freq, void *context);
@@ -47,9 +47,12 @@ static void EnablePushHandler (double timestamp, bool enable, void *context);
 /* To be more generic the PushHandler dhub routine should be in the table */
 static actuator_DhResTypes_t dhubResources[] = {
    {RES_PATH_ENABLE, NULL, DHUBIO_DATA_TYPE_BOOLEAN, "1/0", NULL},
-   {RES_PATH_FREQ_OUTPUT, RES_PATH_FREQ_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "Hz", FreqConfigPushHandler},
-   {RES_PATH_PERIOD_OUTPUT, RES_PATH_PERIOD_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "s", PeriodConfigPushHandler},
-   {RES_PATH_DC_ON_OUTPUT, RES_PATH_DC_ON_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "%", DcOnConfigPushHandler},
+   {RES_PATH_FREQ_OUTPUT, RES_PATH_FREQ_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "Hz",
+    FreqConfigPushHandler},
+   {RES_PATH_PERIOD_OUTPUT, RES_PATH_PERIOD_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "s",
+    PeriodConfigPushHandler},
+   {RES_PATH_DC_ON_OUTPUT, RES_PATH_DC_ON_INPUT, DHUBIO_DATA_TYPE_NUMERIC, "%",
+    DcOnConfigPushHandler},
 };
 
 // The current frequency of square wave output by the rtc chip to the buzzer
@@ -69,78 +72,93 @@ static uint32_t dc_on_timer = 0;
 static uint32_t dc_off_timer = 0;
 static bool buzzer_on = false;
 
-static void SetBuzzer()
+static void SetBuzzer(void)
 {
-    if (freqFp == NULL) {
-    	freqFp = fopen(BuzzerFreqPath, "r+");
-    	if (freqFp == NULL) {
-        	LE_ERROR("Open clkout sysfs file('%s') failed(%d)", BuzzerFreqPath, errno);
-        	return;
-    	}
+    if (freqFp == NULL)
+    {
+        freqFp = fopen(BuzzerFreqPath, "r+");
+        if (freqFp == NULL)
+        {
+            LE_ERROR("Open clkout sysfs file('%s') failed(%d)", BuzzerFreqPath, errno);
+            return;
+        }
     }
 
-    //LE_DEBUG("Clkout frequency %d Hz", current_freq);
+    LE_DEBUG("Clkout frequency %d Hz", current_freq);
 
     if (fprintf(freqFp, "%d", current_freq) == -1)
+    {
         LE_ERROR("Write clkout file('%s') failed (%d)", BuzzerFreqPath, errno);
+    }
 
     if (fflush(freqFp) == -1)
-	LE_ERROR("fflush of /sys/bus/i2c/drivers/rtc-pcf85063/8-0051/clkout_freq failed");
+    {
+        LE_ERROR("fflush of /sys/bus/i2c/drivers/rtc-pcf85063/8-0051/clkout_freq failed");
+    }
 }
 
 static void DcHandler(le_timer_Ref_t DcTimerRef)
 {
     le_timer_Stop(Timer);
-    if (buzzer_on) {
-	le_timer_SetMsInterval(Timer, dc_off_timer);
-	current_freq = TURN_OFF_CLKOUT;
-	SetBuzzer();
-	buzzer_on = false;
-	le_timer_Start(Timer);
+    if (buzzer_on)
+    {
+        le_timer_SetMsInterval(Timer, dc_off_timer);
+        current_freq = TURN_OFF_CLKOUT;
+        SetBuzzer();
+        buzzer_on = false;
+        le_timer_Start(Timer);
     }
-    else if (!buzzer_on) {
-	le_timer_SetMsInterval(Timer, dc_on_timer);
-	current_freq = set_freq;
-	SetBuzzer();
-	buzzer_on = true;
-	le_timer_Start(Timer);
+    else if (!buzzer_on)
+    {
+        le_timer_SetMsInterval(Timer, dc_on_timer);
+        current_freq = set_freq;
+        SetBuzzer();
+        buzzer_on = true;
+        le_timer_Start(Timer);
     }
     else
-	LE_ERROR("Illegal state of buzzer");
+    {
+        LE_ERROR("Illegal state of buzzer");
+    }
 }
 
 static void EnablePushHandler (double timestamp, bool enable, void *context)
 {
+    if (enable)
+    {
+        // We are assuming that set_freq != 0 means that the clkout file is open
+        if (period == 0 || dc_on == 0 || set_freq == 0)
+        {
+            LE_ERROR("Enable buzzer with either these not set: Period, Duty Cycle, Frequency");
+            return;
+        }
 
-    if (enable) {
-	// We are assuming that set_freq != 0 means that the clkout file is open
-	if (period == 0 || dc_on == 0 || set_freq == 0) {
-	    LE_ERROR("Enable buzzer with either these not set: Period, Duty Cycle, Frequency");
-	    return;
-	}
-	
-	dc_on_timer = ((double) dc_on)/100 * period * 1000;
-	dc_off_timer = ((100 - ((double) dc_on))/100) * period * 1000;
-	LE_INFO("period: %d dc_on: %d dc_on_timer: %u dc_off_timer: %u", period, dc_on, dc_on_timer, dc_off_timer);
-    	le_timer_SetMsInterval(Timer, dc_on_timer);
+        dc_on_timer = ((double) dc_on)/100 * period * 1000;
+        dc_off_timer = ((100 - ((double) dc_on))/100) * period * 1000;
+        LE_INFO("period: %d dc_on: %d dc_on_timer: %u dc_off_timer: %u", period, dc_on, dc_on_timer,
+                dc_off_timer);
+        le_timer_SetMsInterval(Timer, dc_on_timer);
 
-	// We start on the duty cycle high
-	buzzer_on = true;
-	if (set_freq == TURN_OFF_CLKOUT) {
-	    LE_ERROR("Turning on buzzer with no frequency");
-	    return;
-	}
-	current_freq = set_freq;
-	SetBuzzer();
-	le_timer_Start(Timer);
+        // We start on the duty cycle high
+        buzzer_on = true;
+        if (set_freq == TURN_OFF_CLKOUT)
+        {
+            LE_ERROR("Turning on buzzer with no frequency");
+            return;
+        }
+        current_freq = set_freq;
+        SetBuzzer();
+        le_timer_Start(Timer);
     }
 
-    if (!enable) {
-	le_timer_Stop(Timer);
-	if (current_freq != TURN_OFF_CLKOUT) {
-	    current_freq = TURN_OFF_CLKOUT;
-	    SetBuzzer();
-	}
+    if (!enable)
+    {
+        le_timer_Stop(Timer);
+        if (current_freq != TURN_OFF_CLKOUT)
+        {
+            current_freq = TURN_OFF_CLKOUT;
+            SetBuzzer();
+        }
     }
 }
 
@@ -157,9 +175,10 @@ static void PeriodConfigPushHandler (double timestamp, double PeriodConfig, void
     period = (int) PeriodConfig;
 
     // Restricting from 2 sec. to 3600 (i.e. 1 hour)
-    if (period < 2 || period > 3600) {
-	LE_ERROR("Received invalid PeriodConfig for the Buzzer Duty Cycle - must be between 2 & 3600");
-	return;
+    if (period < 2 || period > 3600)
+    {
+        LE_ERROR("Received invalid PeriodConfig for the Buzzer Duty Cycle - must be between 2 & 3600");
+        return;
     }
     dhubIO_PushNumeric(RES_PATH_PERIOD_INPUT, DHUBIO_NOW, PeriodConfig);
 }
@@ -168,9 +187,10 @@ static void DcOnConfigPushHandler (double timestamp, double DcOnInterval, void *
 {
     dc_on = (int) DcOnInterval;
 
-    if (dc_on < 1 || dc_on > 99) {
-	LE_ERROR("Received invalid DcOnInterval for the Buzzer Duty Cycle - must be between 1 & 99");
-	return;
+    if (dc_on < 1 || dc_on > 99)
+    {
+        LE_ERROR("Received invalid DcOnInterval for the Buzzer Duty Cycle - must be between 1 & 99");
+        return;
     }
 
     dhubIO_PushNumeric(RES_PATH_DC_ON_INPUT, DHUBIO_NOW, DcOnInterval);
@@ -180,45 +200,54 @@ static le_result_t actuator_DhRegister (void)
 {
     le_result_t result = LE_OK;
 
-    
-    for (int i = 0; i < ARRAY_SIZE(dhubResources); i++) {
-	if (dhubResources[i].OutputPath != NULL)
-            result = dhubIO_CreateOutput(dhubResources[i].OutputPath, 
-                                 dhubResources[i].dataType, 
-                                 dhubResources[i].units);
-        if (LE_OK != result) {
+
+    for (int i = 0; i < ARRAY_SIZE(dhubResources); i++)
+    {
+        if (dhubResources[i].OutputPath != NULL)
+            result = dhubIO_CreateOutput(dhubResources[i].OutputPath,
+                                         dhubResources[i].dataType,
+                                         dhubResources[i].units);
+        if (LE_OK != result)
+        {
             LE_ERROR("Failed to create output resource %s", dhubResources[i].OutputPath);
             break;
         }
 
-	dhubIO_MarkOptional(dhubResources[i].OutputPath);
+        dhubIO_MarkOptional(dhubResources[i].OutputPath);
 
-	/* We try to create an Input to keep all listeners apprised of the value change */
-	if (dhubResources[i].InputPath != NULL)
-       	    LE_ASSERT(LE_OK == dhubIO_CreateInput(dhubResources[i].InputPath, dhubResources[i].dataType, ""));
+        /* We try to create an Input to keep all listeners apprised of the value change */
+        if (dhubResources[i].InputPath != NULL)
+        {
+            LE_ASSERT(LE_OK == dhubIO_CreateInput(dhubResources[i].InputPath,
+                                                  dhubResources[i].dataType, ""));
+        }
 
-	/* To be more generic the PushHandler dhub routine should be in the table */
-	if (dhubResources[i].OutputHandler != NULL) {
-		//LE_INFO("Adding handler for %s", dhubResources[i].OutputPath);
-		dhubResources[i].ref = dhubIO_AddNumericPushHandler(dhubResources[i].OutputPath,
-			dhubResources[i].OutputHandler, NULL);
-		if (NULL == dhubResources[i].ref) {
-            	    LE_ERROR("Failed to add handler for output resource %s", dhubResources[i].OutputPath);
-            	    result = LE_FAULT;
-            	    break;
-        	}
-	}
-	
+        /* To be more generic the PushHandler dhub routine should be in the table */
+        if (dhubResources[i].OutputHandler != NULL)
+        {
+            //LE_INFO("Adding handler for %s", dhubResources[i].OutputPath);
+            dhubResources[i].ref = dhubIO_AddNumericPushHandler(
+                dhubResources[i].OutputPath, dhubResources[i].OutputHandler, NULL);
+            if (NULL == dhubResources[i].ref)
+            {
+                LE_ERROR("Failed to add handler for output resource %s", dhubResources[i].OutputPath);
+                result = LE_FAULT;
+                break;
+            }
+        }
+
     }
 
     // Need seperate code for Bool Push Handler - TODO fix with union
-    if (result == LE_OK) {
-    	dhubResources[0].ref = dhubIO_AddBooleanPushHandler(dhubResources[0].OutputPath,
-				EnablePushHandler, NULL);
-	if (NULL == dhubResources[0].ref) {
+    if (result == LE_OK)
+    {
+        dhubResources[0].ref = dhubIO_AddBooleanPushHandler(dhubResources[0].OutputPath,
+                                                            EnablePushHandler, NULL);
+        if (NULL == dhubResources[0].ref)
+        {
             LE_ERROR("Failed to add handler for output resource %s", dhubResources[0].OutputPath);
             result = LE_FAULT;
-	}
+        }
     }
 
     return result;
@@ -233,7 +262,8 @@ static void actuator_SigTermHandler (int pSigNum)
 {
     LE_INFO("Remove buzzer resources");
 
-    for (int i = 0; i < ARRAY_SIZE(dhubResources); i++) {
+    for (int i = 0; i < ARRAY_SIZE(dhubResources); i++)
+    {
         dhubIO_DeleteResource(dhubResources[i].OutputPath);
         dhubIO_DeleteResource(dhubResources[i].InputPath);
     }
@@ -255,6 +285,4 @@ COMPONENT_INIT
     Timer = le_timer_Create("Buzzer Timer");
     le_timer_SetRepeat(Timer, 0);
     le_timer_SetHandler(Timer, DcHandler);
-
 }
-
